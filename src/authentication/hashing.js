07ad4ac906d5eb2edf0a4aa195e80ca7864a8f1c
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-19 18:36:33 +0100

    Feature: implement password hashing using bcrypt

--- src/authentication/hashing.js
+++ src/authentication/hashing.js
@@ -1,8 +1,20 @@
-const hashPassword = async (plainTextPassword) => {
-  // To be implemented
-  return { plainTextPassword };
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+const algorithm = "bcrypt";
+
+const createPasswordHash = async (plainTextPassword) => {
+  const hash = await bcrypt.hash(plainTextPassword, saltRounds);
+  const hashedPassword = {
+    hash,
+    algorithm,
+    updatedAt: new Date(),
+    failedLoginAttempts: 0,
+    lockUntil: null,
+  };
+  return hashedPassword;
 };
 
 module.exports = {
-  hashPassword,
+  createPasswordHash,
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const { parseAndValidateUser } = require("../validation/validation.js");
-const { hashPassword } = require("../authentication/hashing.js");
+const { createPasswordHash } = require("../authentication/hashing.js");
 const { addNewUser } = require("../database.js");
 
 router.post("/new-user", async (req, res) => {
   const { userData, plainTextPassword } = parseAndValidateUser(req);
-  const hashedPassword = await hashPassword(plainTextPassword);
-  const newUser = { ...userData, password: hashedPassword };
+  const passwordHash = await createPasswordHash(plainTextPassword);
+  const newUser = { ...userData, password: passwordHash };
   const newUserID = await addNewUser(newUser);
   res.status(201).send(`New user ID: ${newUserID}`);
 });
+
+module.exports = router;
 
 // {
 //   "username": "fastfeet",
@@ -25,5 +27,3 @@ router.post("/new-user", async (req, res) => {
 //     "weightKg": 72
 //   }
 // }
-
-module.exports = router;
