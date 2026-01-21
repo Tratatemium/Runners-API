d98d65e04cf3ca03fced3e1886e33d549e4f5b73
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-21 19:01:47 +0100

    Feature: await parseAndValidateUser in postNewUser for proper async handling

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -3,7 +3,7 @@ const { createPasswordHash } = require("../authentication/hashing.js");
 const { addNewUser } = require("../database.js");
 
 const postNewUser = async (req, res) => {
-  const { userData, plainTextPassword } = parseAndValidateUser(req);
+  const { userData, plainTextPassword } = await parseAndValidateUser(req);
   const { passwordHash, passwordMetadata } =
     await createPasswordHash(plainTextPassword);
   const newUser = { ...userData, passwordHash, passwordMetadata };
