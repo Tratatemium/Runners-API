bf654e315b494298bffc3e7f5f1bddcf61e1d700
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-26 16:48:44 +0100

    Feature: add TODO for UUID validation in getUserById function

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -30,6 +30,7 @@ const login = async (req, res) => {
 };
 
 const getUserById = async (req, res) => {
+  // TODO Add validateUUID(req.params.id, "userId") before querying the database, following the same pattern as the runs controller.
   const userData = await db.findUserById(req.params["id"]);
   // TODO This should check for null and return a 404 error instead, similar to the pattern used in src/controllers/runs.controller.js lines 8-12.
   const { _id, credentials, ...safeData } = userData;
