56aa695305e70cba1ac36380d39d0456cc4473b8
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-30 20:13:16 +0100

    Refactor profile validation to streamline checks for optional fields

--- src/middleware/validation/users.validation.js
+++ src/middleware/validation/users.validation.js
@@ -75,14 +75,9 @@ const validateProfile = (req, res, next) => {
   const { firstName, lastName, dateOfBirth, heightCm, weightKg } = profile;
 
   if (firstName != null) validators.validateName(firstName, "firstName");
-
   if (lastName != null) validators.validateName(lastName, "lastName");
-
-  if (dateOfBirth != null)
-    validators.validateISODate(dateOfBirth, "dateOfBirth");
-
+  if (dateOfBirth != null) validators.validateISODate(dateOfBirth, "dateOfBirth");
   if (heightCm != null) validators.validatePositiveNumber(heightCm, "heightCm");
-
   if (weightKg != null) validators.validatePositiveNumber(weightKg, "weightKg");
 
   next();
