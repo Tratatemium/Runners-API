13551b08c1b2c017bdadaf6b8cb1645f6620a394
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-01 19:14:14 +0100

    Fix formatting issues in runs controller for consistency

--- src/controllers/runs.controller.js
+++ src/controllers/runs.controller.js
@@ -1,14 +1,14 @@
-const runsService = require("../services/runs.service.js")
+const runsService = require("../services/runs.service.js");
 
 const postNewRun = async (req, res) => {
   const userId = req.user.userId;
-  const newRun = { userId, ... req.runData }
+  const newRun = { userId, ...req.runData };
   const newRunId = await runsService.createRun(newRun);
   res.status(201).json({ id: newRunId });
 };
 
 const getRunById = async (req, res) => {
-  const runData = await runsService.getRunById(req.params.id)
+  const runData = await runsService.getRunById(req.params.id);
   res.status(200).json(runData);
 };
 
