9580c95e5757024efc3dd762da5fc74398aadf63
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-03 15:13:24 +0100

    Refactor guard middleware to implement checkPermissions function

--- src/middleware/guard.middleware.js
+++ src/middleware/guard.middleware.js
@@ -1,15 +1,38 @@
-// NOTE: currently unused
-const checkOwnership = (param = "id") => {
-  return (req, res, next) => {
-    const resourceId = req.params[param];
+const runsService = require("../services/runs.service.js");
+
+const throwGuardError = (
+  message = "You are not allowed to perform this action.",
+  status = 403,
+) => {
+  const err = new Error(message);
+  err.status = status;
+  throw err;
+};
+
+const checkPremissions = (param = "id", idType) => {
+  return async (req, res, next) => {
     const providedId = req.user.userId;
-    if (providedId !== resourceId) {
-      return res
-        .status(403)
-        .json({ error: "You are not allowed to perform this action." });
+
+    let resourceId;
+    switch (idType) {
+      case "userId":
+        resourceId = req.params[param];
+        break;
+
+      case "runId":
+        const run = await runsService.getRunById(req.params[param]);
+        resourceId = run.userId;
+        break;
+
+      default:
+        throw new Error('idType must be "userId" or "runId".');
     }
+
+    const isOwner = providedId === resourceId;
+    if (!isOwner) throwGuardError();
+
     next();
   };
 };
 
-module.exports = { checkOwnership };
+module.exports = { checkPremissions };
