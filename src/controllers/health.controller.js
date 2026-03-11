cc04a1301ebff9f6275cd771e36b2bc924e16866
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-11 21:02:14 +0100

    Fix health controller to accept request and response parameters

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -2,7 +2,7 @@ const { sendSuccess } = require("../utils/response.utils.js");
 const { checkDBconnection } = require("../utils/db.utils.js");
 const { getUptime } = require("../utils/server.utils.js");
 
-const healthController = () => {
+const healthController = (req, res) => {
   sendSuccess(res, 200, {
     status: "running",
     uptime: getUptime(),
